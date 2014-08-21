9bef860242300cf12270c66f0f8ed983e0506509
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-08-21 16:01:02 +0000

    View and url for downloading csv dataset template.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@7690 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/cartograms/views.py
+++ src/cartograms/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import csv
+from django.http import HttpResponse
+from .models import Cartogram
 
-# Create your views here.
+def cartogram_csv_template(request, pk):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefile.csv"'
+
+    cartogram = Cartogram.objects.get(id=pk)
+
+    writer = csv.writer(response)
+    writer.writerow(['entity_id', 'value'])
+    for entity in cartogram.entities.all():
+        writer.writerow([entity.entity_id, 'null'])
+
+    return response
--- src/choropleth/urls.py
+++ src/choropleth/urls.py
@@ -4,16 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 from choropleths.views import GalleryView
+from cartograms.views import cartogram_csv_template
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'choropleth.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', GalleryView.as_view(), name="gallery"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^datasets/', include('datasets.urls', namespace="datasets")),
     url(r'^choropleths/', include('choropleths.urls', namespace="choropleths")),
+    url(r'^cartogram-template/(?P<pk>[0-9]+)/$', cartogram_csv_template, name="csv-template"),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
