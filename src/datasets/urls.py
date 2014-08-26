d9351348da5c37fc05725476b52b500d8cf2a3bb
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-08-26 21:37:28 +0000

    Added login required decorator to the dataset management view.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@7760 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/datasets/urls.py
+++ src/datasets/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .views import DatasetManagement, DatasetDetail, DatasetCreate, DatasetAPIView, DatasetUpdate, DatasetDelete
 
 urlpatterns = [
-    url(r'^$', DatasetManagement.as_view(), name='dataset-management'),
+    url(r'^$',login_required(DatasetManagement.as_view()), name='dataset-management'),
     url(r'^(?P<pk>[0-9]+)/$', DatasetDetail.as_view(), name='dataset-detail'),
     url(r'^new/$', login_required(DatasetCreate.as_view()), name='dataset-create'),
     url(r'^(?P<pk>[0-9]+)/update/$', login_required(DatasetUpdate.as_view()), name='dataset-update'),
