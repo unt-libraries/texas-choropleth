0551c1eba911d6c5108106dc626f4ad32bec1f50
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-09-09 19:05:03 +0000

    Converted all JS from 4 spaces to 2 spaces.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@7918 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,136 +1,136 @@
 var App = angular.module('App', ['ngResource']);
 
 App.config(function($interpolateProvider, $httpProvider) {
-    // New symbols to prevent interference with Django templating language
-    $interpolateProvider.startSymbol('{$');
-    $interpolateProvider.endSymbol('$}');
+  // New symbols to prevent interference with Django templating language
+  $interpolateProvider.startSymbol('{$');
+  $interpolateProvider.endSymbol('$}');
 
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
 });
 
 // Bootstrap active tab helper
 App.directive('isActive', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            if (attrs.href === window.location.pathname) {
-                element.parent().addClass('active');
-            }
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      if (attrs.href === window.location.pathname) {
+        element.parent().addClass('active');
+      }
+    }
+  };
 });
 
 // Still uses the data-content attr for content
 App.directive('popover', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            options = {
-                trigger: 'hover'
-            };
-            $(element).popover(options);
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      options = {
+        trigger: 'hover'
+      };
+      $(element).popover(options);
+    }
+  };
 });
 
 // Form Validation helper
 App.directive('hasError', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var formGroup = $(element);
-            if (formGroup.find('.errorlist').length > 0) {
-                formGroup.addClass('has-error');
-            }
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var formGroup = $(element);
+      if (formGroup.find('.errorlist').length > 0) {
+        formGroup.addClass('has-error');
+      }
+    }
+  };
 });
 
 // Add a Bootstrap tooltip
 App.directive('tooltip', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            $(element).tooltip();
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      $(element).tooltip();
+    }
+  };
 });
 
 // Helper for notifications and list-item-icons
 App.directive('hasRecords', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            // Compare it with the python bool 
-            if (attrs.records == 'False') {
-                element.addClass('no-records');
-            }
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      // Compare it with the python bool 
+      if (attrs.records == 'False') {
+        element.addClass('no-records');
+      }
+    }
+  };
 });
 
 App.directive('loading', function($http) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            scope.isLoading = function() {
-                return $http.pendingRequests.length > 0;
-            };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      scope.isLoading = function() {
+        return $http.pendingRequests.length > 0;
+      };
 
-            loader = $('#loader');
+      loader = $('#loader');
 
-            scope.$watch(scope.isLoading, function(v) {
-                if (v) {
-                    element.append(loader.show());
-                } else {
-                    $(element).find('#loader').remove();
-                }
-            });
+      scope.$watch(scope.isLoading, function(v) {
+        if (v) {
+          element.append(loader.show());
+        } else {
+          $(element).find('#loader').remove();
         }
-    };
+      });
+    }
+  };
 });
 
 
 App.factory('Choropleth', function($resource) {
-    return $resource('/choropleths/api/:id/ ', { id: '@id' }, {
-        update: {
-            method: 'PUT'
-        }
-    });
+  return $resource('/choropleths/api/:id/ ', { id: '@id' }, {
+    update: {
+      method: 'PUT'
+    }
+  });
 });
 
 // Resource for Dataset
 App.factory('Dataset', function($resource) {
-    return $resource('/datasets/api/:id/ ');
+  return $resource('/datasets/api/:id/ ');
 });
 
 // Resource for Palettes
 App.factory('Palettes', function($resource) {
-    return $resource('/choropleths/api/palettes/:id/ ');
+  return $resource('/choropleths/api/palettes/:id/ ');
 });
 
 
 App.controller('DatasetTableController', function($scope, $http, Dataset) {
 
-    $scope.init = function(id) {
-        Dataset.get({id: id}, function(data) {
-            $scope.dataset = data;
-        });
+  $scope.init = function(id) {
+    Dataset.get({id: id}, function(data) {
+      $scope.dataset = data;
+    });
 
-        $scope.reverse = false;
-        $scope.sortOrder='name';
-    };
-    
+    $scope.reverse = false;
+    $scope.sortOrder='name';
+  };
+  
 });
 
 App.directive('progressBar', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            console.log(attrs);
-            $(element).width(attrs.progressBar);
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      console.log(attrs);
+      $(element).width(attrs.progressBar);
+    }
+  };
 });
 
--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,267 +1,266 @@
 App.directive('choropleth', function($window) {
 
-    var loader = $("#loader");
-    var choropleth = $("#choropleth");
-    choropleth.append(loader.show());
+  var loader = $("#loader");
+  var choropleth = $("#choropleth");
+      choropleth.append(loader.show());
 
-    var width = 600,
-        height = 600;
+  var width = 600,
+      height = 600;
 
-    // D3 Array Object where the data is stored as key : value
-    var rateById = d3.map();
+  // D3 Array Object where the data is stored as key : value
+  var rateById = d3.map();
 
-    // Projection for Map
-    var projection = d3.geo.transverseMercator()
-            .rotate([100 + 20 / 60, -29 - 40 / 60])
-            .scale(3000)
-            .translate([285, 380]);
+  // Projection for Map
+  var projection = d3.geo.transverseMercator()
+      .rotate([100 + 20 / 60, -29 - 40 / 60])
+      .scale(3000)
+      .translate([285, 380]);
 
-    // Declare the path object
-    var path = d3.geo.path()
+  // Declare the path object
+  var path = d3.geo.path()
         .projection(projection);
 
-    return {
-        restrict: "E",
-        scope: {
-            data: '=',
-            domain: '=',
-            range: '=',
-            label: '=',
-            scheme: '=',
-            scale: '=',
-            palette: '=',
-        },
-        link: function (scope, element, attrs) {
-            var scale, svg;
+  return {
+    restrict: "E",
+    scope: {
+      data: '=',
+      domain: '=',
+      range: '=',
+      label: '=',
+      scheme: '=',
+      scale: '=',
+      palette: '=',
+    },
+    link: function (scope, element, attrs) {
+      var scale, svg;
 
-            svg = d3.select(element[0]).append("svg")
-                .attr("width", width)
-                .attr("height", height);
+      svg = d3.select(element[0]).append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-            $('svg').hide();
+      $('svg').hide();
 
-            // Watch for changes in the dataset
-            scope.$watch('data', function(newVals, oldVals) {
-                updateData(newVals);
-            }, true);
+      // Watch for changes in the dataset
+      scope.$watch('data', function(newVals, oldVals) {
+        updateData(newVals);
+      }, true);
 
-            scope.$watchCollection('[scale, range, scheme, palette]', function(newVals, oldVals) {
-                setScale(newVals[0]);
-                updateData(scope.data);
-            });
+      scope.$watchCollection('[scale, range, scheme, palette]', function(newVals, oldVals) {
+        setScale(newVals[0]);
+        updateData(scope.data);
+      });
 
-            function setScale(scaleId) {
-                switch(scaleId) {
-                    // Quantize
-                    case 0:
-                        if (scope.palette && scope.range) {
-                            scale = d3.scale.quantize()
-                                .domain([scope.domain.min, scope.domain.max])
-                                .range(d3.range(scope.range).map(function(i) {
-                                return colorbrewer[scope.palette][scope.range][i];
-                                }));
-                        }
-                        break;
-
-                    // Non-Quantize
-                    case 1:
-                        if (scope.palette && scope.range) {
-                            var palette = colorbrewer[scope.palette][scope.range];
-
-                            var color1 = palette[0];
-                                color2 = palette[palette.length -1];
-
-                            scale = d3.scale.log()
-                                .domain([scope.domain.min, scope.domain.max])
-                                .range([color1, color2])
-                                .clamp(true);
-                            console.log(scope.domain.min, scope.domain.max)
-                        }
-                        break;
-
-                    // No scale
-                    default:
-                        scale = function() {
-                            return "none";
-                        };
-                }
-                return scale;
+      function setScale(scaleId) {
+        switch(scaleId) {
+          // Quantize
+          case 0:
+            if (scope.palette && scope.range) {
+              scale = d3.scale.quantize()
+                .domain([scope.domain.min, scope.domain.max])
+                .range(d3.range(scope.range).map(function(i) {
+                  return colorbrewer[scope.palette][scope.range][i];
+                }));
             }
+            break;
 
-            function updateData(data) {
-                svg.select("#entities").selectAll("path")
-                    .attr('class', null)
-                    .attr('fill', function(d) {
-                        data.forEach(function(d) { rateById.set(d.cartogram_entity, d.value); });
-                        return fill(d);
-                    });
+          // Non-Quantize
+          case 1:
+            if (scope.palette && scope.range) {
+              var palette = colorbrewer[scope.palette][scope.range];
+
+              var color1 = palette[0];
+                color2 = palette[palette.length -1];
+
+              scale = d3.scale.log()
+                .domain([scope.domain.min, scope.domain.max])
+                .range([color1, color2])
+                .clamp(true);
             }
+            break;
 
-            function fill(d) {
-                 value = rateById.get(d.properties.fips);
-                 if (value == null) {
-                     return 'none'
-                 } else {
-                     return scale(rateById.get(d.properties.fips));
-                 }
-
-            }
-
-            function ready(error, texas) {
-              svg.append("g")
-                  .attr("id", "entities")
-                .selectAll("path")
-                 .data(topojson.feature(texas, texas.objects.counties).features)
-                .enter().append("path")
-                  .attr("id", function(d) { return d.properties.fips; })
-                  .attr("fill", function(d) { return fill(d); })
-                  .attr("d", path);
-
-              choropleth.find('#loader').fadeOut('slow', function() {
-                  this.remove();
-                  $('svg').fadeIn('slow');
-              });
-            }
-
-            function render(data) {
-                data.forEach(function(d) {rateById.set(d.cartogram_entity, d.value); });
-                queue()
-                    .defer(d3.json, "/static/JSON/texas.json")
-                    .await(ready);
-            }
-
-            render(scope.data);
+          // No scale
+          default:
+            scale = function() {
+              return "none";
+            };
         }
-    };
+        return scale;
+      }
+
+      function updateData(data) {
+        svg.select("#entities").selectAll("path")
+          .attr('class', null)
+          .attr('fill', function(d) {
+            data.forEach(function(d) { rateById.set(d.cartogram_entity, d.value); });
+            return fill(d);
+          });
+      }
+
+      function fill(d) {
+         value = rateById.get(d.properties.fips);
+         if (value == null) {
+           return 'none'
+         } else {
+           return scale(rateById.get(d.properties.fips));
+         }
+      }
+
+      function ready(error, texas) {
+        svg.append("g")
+          .attr("id", "entities")
+        .selectAll("path")
+         .data(topojson.feature(texas, texas.objects.counties).features)
+        .enter().append("path")
+          .attr("id", function(d) { return d.properties.fips; })
+          .attr("fill", function(d) { return fill(d); })
+          .attr("d", path);
+
+        choropleth.find('#loader').fadeOut('slow', function() {
+          this.remove();
+          $('svg').fadeIn('slow');
+        });
+      }
+
+      function render(data) {
+        data.forEach(function(d) {rateById.set(d.cartogram_entity, d.value); });
+        queue()
+          .defer(d3.json, "/static/JSON/texas.json")
+          .await(ready);
+      }
+
+      render(scope.data);
+    }
+  };
 });
 
 // Live edit and generic rendering directive for Markdown
 App.directive('markdown', function($window) {
-    var converter = new $window.Showdown.converter();
-    return {
-        restrict: 'E',
-        scope: {
-            description: '='
-        },
-        link: function(scope, element, attrs) {
-            function markdownify(html) {
-                var htmlText = converter.makeHtml(html);
-                element.html(htmlText);
-            }
+  var converter = new $window.Showdown.converter();
+  return {
+    restrict: 'E',
+    scope: {
+      description: '='
+    },
+    link: function(scope, element, attrs) {
+      function markdownify(html) {
+        var htmlText = converter.makeHtml(html);
+        element.html(htmlText);
+      }
 
-            // For static viewing
-            if (!scope.description) {
-                markdownify(element.text());
-            }
+      // For static viewing
+      if (!scope.description) {
+        markdownify(element.text());
+      }
 
-            // For live editing
-            scope.$watch('description', function(newVal, oldVal) {
-                if (newVal) {
-                    markdownify(newVal);
-                }
-            });
+      // For live editing
+      scope.$watch('description', function(newVal, oldVal) {
+        if (newVal) {
+          markdownify(newVal);
         }
-    };
+      });
+    }
+  };
 });
 
 App.controller('AbstractController', function AbstractController ($scope, $http, Palettes) {
 
-    $scope.tab = 1;
+  $scope.tab = 1;
 
-    $scope._success = function() {
-        window.location = "/choropleths/";
-    };
+  $scope._success = function() {
+    window.location = "/choropleths/";
+  };
 
-    $scope.rangeOptions = [3, 4, 5, 6, 7, 8, 9];
-    $scope.scales = [
-        {name: 'Quantized', id: 0},
-        {name: 'Logarithmic', id: 1}
-    ];
-    $scope.schemes = [
-        {name: "Sequential", id: 1},
-        {name: "Diverging", id: 2},
-        {name: "Qualitivative", id: 3}
-    ];
+  $scope.rangeOptions = [3, 4, 5, 6, 7, 8, 9];
+  $scope.scales = [
+    {name: 'Quantized', id: 0},
+    {name: 'Logarithmic', id: 1}
+  ];
+  $scope.schemes = [
+    {name: "Sequential", id: 1},
+    {name: "Diverging", id: 2},
+    {name: "Qualitivative", id: 3}
+  ];
 
-    $scope.getSchemePalettes = function(id) {
-        Palettes.query({id: id}, function(data) {
-            $scope.palettes = data;
-            $scope.findPalette($scope.choropleth.palette);
-        });
-    };
+  $scope.getSchemePalettes = function(id) {
+    Palettes.query({id: id}, function(data) {
+      $scope.palettes = data;
+      $scope.findPalette($scope.choropleth.palette);
+    });
+  };
 
-    $scope.findPalette = function(id) {
-        if (0 < $scope.palettes.length) {
+  $scope.findPalette = function(id) {
+    if (0 < $scope.palettes.length) {
 
-            // Set defaults if the choropleth doesn't already have values
-            $scope.palette = $scope.palettes[0];
-            $scope.choropleth.palette = $scope.palette.id;
+      // Set defaults if the choropleth doesn't already have values
+      $scope.palette = $scope.palettes[0];
+      $scope.choropleth.palette = $scope.palette.id;
 
-            // Set palette if the palette id is in the palettes array
-            $scope.palettes.forEach(function (value, index, array) {
-                if (value.id == id) {
-                    $scope.palette = value;
-                    $scope.choropleth.palette = value.id;
-                }
-            });
+      // Set palette if the palette id is in the palettes array
+      $scope.palettes.forEach(function (value, index, array) {
+        if (value.id == id) {
+          $scope.palette = value;
+          $scope.choropleth.palette = value.id;
         }
-    };
+      });
+    }
+  };
 });
 
 App.controller('ViewController', function ViewController ($scope, $controller, Choropleth, Dataset, Palettes ) {
-    $controller('AbstractController', {$scope: $scope});
+  $controller('AbstractController', {$scope: $scope});
 
-    $scope.init = function(id) {
-        Choropleth.get({id: id}, function (data) {
-            $scope.choropleth = data;
+  $scope.init = function(id) {
+    Choropleth.get({id: id}, function (data) {
+      $scope.choropleth = data;
 
-            Dataset.get({id: data.dataset}, function(data) {
-                $scope.dataset = data;
-                $scope.scales = data.scale_options
-                $scope.hasData = true;
-            });
+      Dataset.get({id: data.dataset}, function(data) {
+        $scope.dataset = data;
+        $scope.scales = data.scale_options
+        $scope.hasData = true;
+      });
 
-            Palettes.query({id: data.scheme}, function (data) {
-                $scope.palettes = data;
-                $scope.findPalette($scope.choropleth.palette);
-            });
-        });
-    };
+      Palettes.query({id: data.scheme}, function (data) {
+        $scope.palettes = data;
+        $scope.findPalette($scope.choropleth.palette);
+      });
+    });
+  };
 });
 
 App.controller('EditController', function EditController ($scope, $controller, Choropleth, Dataset, Palettes) {
-    $controller('ViewController', {$scope: $scope});
+  $controller('ViewController', {$scope: $scope});
 
-    $scope.submit = function() {
-        $scope.choropleth.$update($scope._success);
-    };
+  $scope.submit = function() {
+    $scope.choropleth.$update($scope._success);
+  };
 
-    $scope.delete = function() {
-        $scope.choropleth.$delete($scope._success);
-    };
+  $scope.delete = function() {
+    $scope.choropleth.$delete($scope._success);
+  };
 });
+
 App.controller('MappCtrl', function MappCtrl ($scope, $controller, Choropleth, Dataset, Palettes) {
-    $controller('AbstractController', {$scope: $scope});
+  $controller('AbstractController', {$scope: $scope});
 
-    $scope.init = function(id) {
-        $scope.choropleth = new Choropleth();
+  $scope.init = function(id) {
+    $scope.choropleth = new Choropleth();
 
-        Dataset.get({id: id}, function(data) {
-            $scope.dataset = data;
-            $scope.choropleth.dataset = data.id;
-            $scope.choropleth.name = data.name;
-            $scope.choropleth.data_classes = 3;
-            $scope.scales = data.scale_options;
-            $scope.hasData = true;
-        });
+    Dataset.get({id: id}, function(data) {
+      $scope.dataset = data;
+      $scope.choropleth.dataset = data.id;
+      $scope.choropleth.name = data.name;
+      $scope.choropleth.data_classes = 3;
+      $scope.scales = data.scale_options;
+      $scope.hasData = true;
+    });
 
-        Palettes.query({id: 1}, function(data) {
-            $scope.palettes = data;
-        });
-    };
+    Palettes.query({id: 1}, function(data) {
+      $scope.palettes = data;
+    });
+  };
 
-    $scope.submit = function() {
-        $scope.choropleth.$save($scope._success);
-    };
+  $scope.submit = function() {
+    $scope.choropleth.$save($scope._success);
+  };
 });
 
