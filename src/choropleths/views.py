4670c1717c35a7e7360643b8336b8d660adc4e98
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-09-09 19:58:46 +0000

    General refactoring. Added GetPublishedObjectMixin. Cleaned up import statements. Group API views together.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@7922 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/choropleths/views.py
+++ src/choropleths/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView
+from rest_framework import generics
 from rest_framework import viewsets
-from django.views.generic import DetailView, ListView, TemplateView
-from django.views.generic.edit import CreateView
+from django.views import generic
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import User
 from .models import Choropleth, Palette
@@ -10,27 +9,30 @@ from .serializers import ChoroplethSerializer, PaletteSerializer
 from datasets.serializers import DatasetSerializer
 from datasets.models import Dataset
 
-class GalleryView(ListView):
+
+class GetPublishedObjectMixin(object):
+    """
+    Get Object Mixin
+
+    Retrieves the object if the request user is the owner, or if
+    the object is published. Otherwise returns status 403
+    """
+    def get_object(self, **kwargs):
+        gotten_object = super(GetPublishedObjectMixin, self).get_object(**kwargs)
+        if gotten_object.owner != self.request.user:
+            if gotten_object.published == 0:
+                raise PermissionDenied()
+        return gotten_object
+
+
+class GalleryView(generic.ListView):
     model = Choropleth
     template_name = "choropleths/gallery.html"
     paginate_by = 10
     queryset = Choropleth.objects.filter(published=1).order_by('-created_at', 'name')
 
-class ChoroplethAPIView(RetrieveAPIView):
-    model = Choropleth
-    serializer_class = ChoroplethSerializer
 
-
-class PaletteAPIView(ListAPIView):
-    model = Palette
-    serializer_class = PaletteSerializer
-
-    def get_queryset(self):
-        palettes = Palette.objects.filter(scheme=self.kwargs['pk'])
-        return palettes
-
-
-class ChoroplethList(ListView):
+class ChoroplethList(generic.ListView):
     model = Choropleth
     template_name = 'choropleths/choropleth_list.html'
     paginate_by = 10
@@ -40,42 +42,31 @@ class ChoroplethList(ListView):
         return Choropleth.objects.filter(owner=user).order_by('-modified_at')
 
 
-class ChoroplethDetail(DetailView):
+class ChoroplethDetail(GetPublishedObjectMixin, generic.DetailView):
     model = Choropleth
     template_name = 'choropleths/choropleth_detail.html'
-
-    def get_object(self, **kwargs):
-        choropleth = super(ChoroplethDetail, self).get_object(**kwargs)
-        if choropleth.owner != self.request.user:
-            if choropleth.published == 0:
-                raise PermissionDenied()
-        return choropleth
     
 
-class ChoroplethView(DetailView):
+class ChoroplethView(GetPublishedObjectMixin, generic.DetailView):
     template_name = 'choropleths/choropleth_view.html'
     model = Choropleth 
 
-    def get_object(self, **kwargs):
-        choropleth = super(ChoroplethView, self).get_object(**kwargs)
-        if choropleth.owner != self.request.user:
-            if choropleth.published == 0:
-                raise PermissionDenied()
-        return choropleth
 
-
-class ChoroplethEdit(DetailView):
+class ChoroplethEdit(GetPublishedObjectMixin, generic.DetailView):
     template_name = "choropleths/choropleth_edit.html"
     model = Choropleth
 
     def get_object(self, **kwargs):
+        """
+        Only the owner may show get a status 200
+        """
         choropleth = super(ChoroplethEdit, self).get_object(**kwargs)
         if choropleth.owner != self.request.user:
             raise PermissionDenied()
         return choropleth
 
 
-class ChoroplethCreate(TemplateView):
+class ChoroplethCreate(generic.TemplateView):
     template_name = "choropleths/choropleth_create.html"
     
     def get_context_data(self, **kwargs):
@@ -88,6 +79,14 @@ class ChoroplethCreate(TemplateView):
         else:
             raise PermissionDenied()
 
+
+class PaletteAPIView(generics.ListAPIView):
+    model = Palette
+    serializer_class = PaletteSerializer
+
+    def get_queryset(self):
+        palettes = Palette.objects.filter(scheme=self.kwargs['pk'])
+        return palettes
 
 class ChoroplethAPI(viewsets.ModelViewSet):
     model = Choropleth
