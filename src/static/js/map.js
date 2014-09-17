36f1548b581219ef55d3bfd719542bc78d6227f0
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-09-17 22:00:27 +0000

    Preliminary gradient legend for logarithmic scale.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8017 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -62,6 +62,63 @@ App.directive('choropleth', function($http) {
         update(scope.dataset.records);
       });
 
+      function legend(color1, color2) {
+        var w =10, h = 400;
+
+        d3.select('g#legend').remove();
+
+        var key = d3.select("svg")
+            .append("g")
+            .attr('id', 'legend')
+            .attr("width", w)
+            .attr("height", h);
+
+        var legend = key.append("defs")
+            .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "100%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+        legend.append("stop")
+          .attr("offset", "0%")
+          .attr("stop-color", color2)
+          .attr("stop-opacity", 1);
+
+        legend.append("stop")
+        .attr("offset", "100%")
+          .attr("stop-color", color1)
+          .attr("stop-opacity", 1);
+
+        key.append("rect")
+          .attr("width", w) 
+          .attr("height", h - 100)
+          .style("fill", "url(#gradient)")
+          .attr("transform", "translate(25,10)");
+
+        var y = d3.scale.linear()
+          .range([300, 0])
+          .domain([1, 100]);
+
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("right");
+
+        key.append("g")
+          .attr("class", "y axis")
+          .attr("transform", "translate(41,10)")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 30)
+          .attr("dy", ".80em")
+          .style("text-anchor", "end")
+          .text(scope.dataset.label);
+
+      }
+
       // Determines correct scale for the current ScaleID
       function setScale(scaleId) {
         var range = scope.choropleth.data_classes;
@@ -96,6 +153,8 @@ App.directive('choropleth', function($http) {
                 .domain([min, max])
                 .range([color1, color2])
                 .clamp(true);
+
+              legend(color1, color2)
             }
             break;
 
