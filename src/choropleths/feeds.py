afddf584e0dabe5a9c7388374abacb867a63ed5b
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-09-22 21:50:31 +0000

    Choropleth feed.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8058 f04a3889-0f81-4131-97fb-bc517d1f583d

---
+++ src/choropleths/feeds.py
@@ -0,0 +1,22 @@
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from choropleths.models import Choropleth
+
+class ChoroplethFeed(Feed):
+    title = "Choropleths"
+    link = '/feed/'
+
+    def items(self):
+        return Choropleth.objects \
+                .filter(published=1) \
+                .order_by('-created_at') \
+                .select_related('dataset')[0:10]
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return item.dataset.description
+
+    def item_link(self, item):
+        return reverse('choropleths:choropleth-view', args=[item.id])
