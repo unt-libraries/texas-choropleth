944f0012064e921e2fc97d41e476220714a3de88
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-09-22 21:39:30 +0000

    Added login required decorator to necessary urls.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8055 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/choropleths/urls.py
+++ src/choropleths/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ChoroplethList.as_view(), name='choropleth-list'),
+    url(r'^$', login_required(views.ChoroplethList.as_view()), name='choropleth-list'),
     url(r'^(?P<pk>[0-9]+)/$', views.ChoroplethDetail.as_view(), name='choropleth-detail'),
-    url(r'^create/(?P<pk>[0-9]+)/$', views.ChoroplethCreate.as_view(), name='choropleth-create'),
-    url(r'^(?P<pk>[0-9]+)/edit/$', views.ChoroplethEdit.as_view(), name='choropleth-edit'),
+    url(r'^create/(?P<pk>[0-9]+)/$', login_required(views.ChoroplethCreate.as_view()), name='choropleth-create'),
+    url(r'^(?P<pk>[0-9]+)/edit/$', login_required(views.ChoroplethEdit.as_view()), name='choropleth-edit'),
     url(r'^(?P<pk>[0-9]+)/view/$', views.ChoroplethView.as_view(), name='choropleth-view'),
     url(r'^(?P<pk>[0-9]+)/export/$', views.ChoroplethExport.as_view(), name='choropleth-export'),
     url(r'^api/$', views.ChoroplethAPI.as_view({'get':'list', 'post': 'create'}), name='api-create'),
