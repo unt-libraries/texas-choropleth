0fa6ee483ed06e06623323cd754093dbf6534dab
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-10-31 20:10:44 +0000

    No longer aliasing core.views as it is not necessary.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8340 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 from cartograms.views import cartogram_csv_template
-from core import views as core_views
+from core import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', core_views.GalleryView.as_view(), name="gallery"),
-    url(r'^help/$', core_views.HelpView.as_view(), name="help"),
+    url(r'^$', views.GalleryView.as_view(), name="gallery"),
+    url(r'^help/$', views.HelpView.as_view(), name="help"),
     url(r'^cartogram-template/(?P<pk>[0-9]+)/$', cartogram_csv_template, name="csv-template"),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}, name='logout'),
-    url(r'^register/$', core_views.RegisterView.as_view(), name='register'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^reset/', include('password_reset.urls')),
     url(r'^api/', include('core.api.urls', namespace="api")),
 )
