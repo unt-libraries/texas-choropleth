e19819f95310a8077137ca4b888ab8ae35b5c478
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-07 16:27:23 +0000

    Whitespace.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8385 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/datasets/urls.py
+++ src/datasets/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$',login_required(views.DatasetManagement.as_view()), name='management'),
+    url(r'^$', login_required(views.DatasetManagement.as_view()), name='management'),
     url(r'^(?P<pk>[0-9]+)/$', views.DatasetDetail.as_view(), name='detail'),
     url(r'^new/$', login_required(views.DatasetCreate.as_view()), name='create'),
     url(r'^(?P<pk>[0-9]+)/update/$', login_required(views.DatasetUpdate.as_view()), name='update'),
