ca406b05e16b36bc0a90ce80df5cdbbdebd19ab6
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-10 19:25:47 +0000

    Add some helpful output.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8405 f04a3889-0f81-4131-97fb-bc517d1f583d

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,15 @@
 from fabric.api import local
+from fabric.colors import green
 
 
 def build():
+    print(green("=====[ Installing Bowering Components ]====="))
     local('bower install --allow-root --config.interactive=false')
+    print(green("\n=====[ Syncing Database ]====="))
     local('./src/manage.py syncdb --noinput')
+    print(green("\n=====[ Running Database Migrations ]====="))
     local('./src/manage.py migrate')
+    print(green("\n=====[ Loading Fixtures ]====="))
     local('./src/manage.py loaddata texas.json')
     local('./src/manage.py loaddata licenses.json')
     local('./src/manage.py loaddata palettes.json')
