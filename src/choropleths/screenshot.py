0ffc5b0060a3bbea5da1a93f2f251e315d718dad
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-11 18:23:05 +0000

    Removed unused imports. Removed __main__ check. Formatting.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8420 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/choropleths/screenshot.py
+++ src/choropleths/screenshot.py
@@ -2,8 +2,6 @@ import os
 from django.conf import settings
 from subprocess import Popen, PIPE
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import selenium.webdriver.support.ui as ui
 
 abspath = lambda *p: os.path.abspath(os.path.join(*p))
@@ -18,11 +16,13 @@ def execute_command(command):
 
 def do_screen_capturing(url, screen_path, width, height):
     print "Capturing screen.."
-    driver = webdriver.PhantomJS(service_log_path=os.path.join(settings.LOG_DIR, 'ghostdriver.log'))
+    driver = webdriver.PhantomJS(
+        service_log_path=os.path.join(settings.LOG_DIR, 'ghostdriver.log'))
     # it save service log file in same directory
     # if you want to have log file stored else where
     # initialize the webdriver.PhantomJS() as
-    # driver = webdriver.PhantomJS(service_log_path='/var/log/phantomjs/ghostdriver.log')
+    # driver = webdriver.PhantomJS(
+    #     service_log_path='/var/log/phantomjs/ghostdriver.log')
     driver.set_script_timeout(30)
     if width and height:
         driver.set_window_size(width, height)
@@ -32,11 +32,10 @@ def do_screen_capturing(url, screen_path, width, height):
     driver.get(url)
     print driver.current_url
     try:
-        svg = wait.until(lambda driver: driver.find_element_by_tag_name('svg'))
+        wait.until(lambda driver: driver.find_element_by_tag_name('svg'))
         driver.save_screenshot(screen_path)
     finally:
         driver.quit()
-
 
 
 def do_crop(params):
@@ -64,20 +63,28 @@ def do_thumbnail(params):
 
 def get_screen_shot(**kwargs):
     url = kwargs['url']
-    width = int(kwargs.get('width', 650)) # screen width to capture
-    height = int(kwargs.get('height', 600)) # screen height to capture
-    filename = kwargs.get('filename', 'screen.png') # file name e.g. screen.png
-    path = kwargs.get('path', ROOT) # directory path to store screen
+    width = int(kwargs.get('width', 650))
+    height = int(kwargs.get('height', 600))
+    filename = kwargs.get('filename', 'screen.png')
 
-    crop = kwargs.get('crop', False) # crop the captured screen
-    crop_width = int(kwargs.get('crop_width', width)) # the width of crop screen
-    crop_height = int(kwargs.get('crop_height', height)) # the height of crop screen
-    crop_replace = kwargs.get('crop_replace', False) # does crop image replace original screen capture?
+    # directory path to store screen
+    path = kwargs.get('path', ROOT)
 
-    thumbnail = kwargs.get('thumbnail', False) # generate thumbnail from screen, requires crop=True
-    thumbnail_width = int(kwargs.get('thumbnail_width', width)) # the width of thumbnail
-    thumbnail_height = int(kwargs.get('thumbnail_height', height)) # the height of thumbnail
-    thumbnail_replace = kwargs.get('thumbnail_replace', False) # does thumbnail image replace crop image?
+    # crop the captured screen
+    crop = kwargs.get('crop', False)
+    crop_width = int(kwargs.get('crop_width', width))
+    crop_height = int(kwargs.get('crop_height', height))
+
+    # does crop image replace original screen capture?
+    crop_replace = kwargs.get('crop_replace', False)
+
+    # generate thumbnail from screen, requires crop=True
+    thumbnail = kwargs.get('thumbnail', False)
+    thumbnail_width = int(kwargs.get('thumbnail_width', width))
+    thumbnail_height = int(kwargs.get('thumbnail_height', height))
+
+    # does thumbnail image replace crop image?
+    thumbnail_replace = kwargs.get('thumbnail_replace', False)
 
     screen_path = abspath(path, filename)
     crop_path = thumbnail_path = screen_path
@@ -103,22 +110,3 @@ def get_screen_shot(**kwargs):
                 'thumbnail_path': thumbnail_path, 'crop_path': crop_path}
             do_thumbnail(params)
     return screen_path, crop_path, thumbnail_path
-
-
-if __name__ == '__main__':
-    '''
-        Requirements:
-        Install NodeJS
-        Using Node's package manager install phantomjs: npm -g install phantomjs
-        install selenium (in your virtualenv, if you are using that)
-        install imageMagick
-        add phantomjs to system path (on windows)
-    '''
-
-    url = 'http://172.17.42.1:8000/choropleths/12/export/'
-    screen_path, crop_path, thumbnail_path = get_screen_shot(
-        url=url, filename='sof.png',
-        crop=True, crop_replace=False,
-        thumbnail=True, thumbnail_replace=False,
-        thumbnail_width=200, thumbnail_height=150,
-    )
