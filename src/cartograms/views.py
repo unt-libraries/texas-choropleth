ce54230e0ed67fcc382dee0e85dfc414b445852e
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-11 18:23:28 +0000

    Fixed formatting. Removed extra whitespace.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8422 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/cartograms/views.py
+++ src/cartograms/views.py
@@ -2,14 +2,15 @@ import csv
 from django.http import HttpResponse
 from .models import Cartogram
 
+
 def cartogram_csv_template(request, pk):
     cartogram = Cartogram.objects.get(id=pk)
 
-    content_disposition = 'attachment; filename="{}.csv"'.format(cartogram.name.lower())
+    content_disposition = 'attachment; filename="{}.csv"'.format(
+        cartogram.name.lower())
 
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = content_disposition
-
 
     writer = csv.writer(response)
     writer.writerow(['fips', 'name', 'value'])
