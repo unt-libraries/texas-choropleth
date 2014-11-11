2811d1c5731a0a87b546267520b450a03dd8b8c3
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-11 18:19:53 +0000

    manage.py should set the local settings as default.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8408 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/manage.py
+++ src/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "texas_choropleth.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "texas_choropleth.settings.local")
 
     from django.core.management import execute_from_command_line
 
