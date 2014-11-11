a4f357281c720033568c51fad81be34be81535e3
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-11 18:21:47 +0000

    Better formatting.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8414 f04a3889-0f81-4131-97fb-bc517d1f583d

--- src/choropleths/feeds.py
+++ src/choropleths/feeds.py
@@ -2,15 +2,17 @@ from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from choropleths.models import Choropleth
 
+
 class ChoroplethFeed(Feed):
     title = "Choropleths"
     link = '/feed/'
 
     def items(self):
-        return Choropleth.objects \
-                .filter(published=1) \
-                .order_by('-created_at') \
-                .select_related('dataset')[0:10]
+        return (
+            Choropleth.objects
+            .filter(published=1)
+            .order_by('-created_at')
+            .select_related('dataset')[0:10])
 
     def item_title(self, item):
         return item.name
