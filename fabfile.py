ea6b0de459b76d3646790194034fa91f5b6b9945
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-11 18:19:10 +0000

    Seperated the build operation in to two task. One for dev another for production.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8406 f04a3889-0f81-4131-97fb-bc517d1f583d

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,35 @@
-from fabric.api import local
+from fabric.api import local, run
 from fabric.colors import green
+from fabric.contrib import django
+from fabric.decorators import task
 
 
 def build():
-    print(green("=====[ Installing Bowering Components ]====="))
+    print(green("[ Installing Bowering Components ]"))
     local('bower install --allow-root --config.interactive=false')
-    print(green("\n=====[ Syncing Database ]====="))
+
+    print(green("\n[ Syncing Database ]"))
     local('./src/manage.py syncdb --noinput')
-    print(green("\n=====[ Running Database Migrations ]====="))
+
+    print(green("\n[ Running Database Migrations ]"))
     local('./src/manage.py migrate')
-    print(green("\n=====[ Loading Fixtures ]====="))
+
+    print(green("\n[ Loading Fixtures ]"))
     local('./src/manage.py loaddata texas.json')
     local('./src/manage.py loaddata licenses.json')
     local('./src/manage.py loaddata palettes.json')
+
+
+@task
+def build_dev():
+    django.settings_module('texas_choropleth.settings.local')
+    build()
+
+
+@task
+def build_prod():
+    django.settings_module('texas_choropleth.settings.production')
+    build()
+
+    print(green("\n [ Collecting Staticfiles ]"))
+    local('./src/manage.py collectstatic --noinput')
