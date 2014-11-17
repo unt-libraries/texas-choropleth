78dc99fcd972550c37e13bcd23a7c270d8c71310
dmk0094 <dmk0094@f04a3889-0f81-4131-97fb-bc517d1f583d>
2014-11-17 22:01:28 +0000

    Added a manage functions and docstrings.
    git-svn-id: svn+ssh://digitalprojects.library.unt.edu/var/lib/svn/trac/choropleth/trunk@8444 f04a3889-0f81-4131-97fb-bc517d1f583d

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,34 @@ from fabric.decorators import task
 
 
 @task
+def local_manage(param=''):
+    """
+    A wrapper for executing Django commands from the docker host or client.
+    """
+    local('fig run --rm web ./src/manage.py {0}'.format(param))
+
+
+@task
+def manage(param=''):
+    """
+    Wrapper for Django commands.
+    """
+    local('./src/manage.py {0}'.format(param))
+
+
+@task
 def run_tests(test='src'):
+    """
+    Wrapper for running tests.
+    """
     django.settings_module('texas_choropleth.settings.test')
     local('./src/manage.py test {0}'.format(test))
 
 
 def build():
+    """
+    Base function for building the application.
+    """
     print(green("[ Installing Bowering Components ]"))
     local('bower install --allow-root --config.interactive=false')
 
@@ -27,12 +49,18 @@ def build():
 
 @task
 def build_dev():
+    """
+    Build the application for a development environment.
+    """
     django.settings_module('texas_choropleth.settings.local')
     build()
 
 
 @task
 def build_prod():
+    """
+    Build the application for the production environment.
+    """
     django.settings_module('texas_choropleth.settings.production')
     build()
 
